c066748a6eb7c0f85c9ebd6f3bb05bffd678dce9
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-06-05 13:26:35 +0900

    3-5 動作確認

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, mageMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
 from io import BytesIO
 from azure.cognitiveservices.vision.face import FaceClient
@@ -59,52 +59,15 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-		try:
-				# ----- A -----
-        # メッセージIDを受け取る
-        message_id = event.message.id
-        # メッセージIDに含まれるmessage_contentを抽出する
-        message_content = line_bot_api.get_message_content(message_id)
-        # contentの画像データをバイナリデータとして扱えるようにする
-        image = BytesIO(message_content.content)
-
-				# ----- B -----
-        # Detect from streamで顔検出
-        detected_faces = face_client.face.detect_with_stream(image)
-        print(detected_faces)
-        # 検出結果に応じて処理を分ける
-        if detected_faces != []:
-            # 顔検出ができたら顔認証を行う
-            valified = face_client.face.verify_face_to_person(
-                face_id = detected_faces[0].face_id,
-                person_group_id = PERSON_GROUP_ID,
-                person_id = PERSON_ID_AUDREY
-            )
-            # 認証結果に応じて処理を変える
-            if valified:
-                if valified.is_identical:
-                    # 顔認証が一致した場合（スコアもつけて返す）
-                    text = 'この写真はオードリーヘップバーンです(score:{:.3f})'.format(valified.confidence)
-                else:
-                    # 顔認証が一致した場合（スコアもつけて返す）
-                    text = 'この写真はオードリーヘップバーンではありません(score:{:.3f})'.format(valified.confidence)
-            else:
-                text = '識別できませんでした。'
-        else:
-            # 検出されない場合のメッセージ
-            text = "写真から顔が検出できませんでした。他の画像で試してください。"
-    except:
-        # エラー時のメッセージ
-        text = "error"
-
-		# ----- C -----
     # LINEチャネルを通じてメッセージを返答
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=text)
+        TextSendMessage(text='画像です')
     )
+
 
 if __name__ == "__main__":
     app.run()
