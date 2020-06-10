21ce7eedbba575a70c65b1a3827935bed690601b
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-06-11 02:29:34 +0900

    fix 5-2

--- app.py
+++ app.py
@@ -10,11 +10,22 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
+from linebot.models import ImageMessage
+from io import BytesIO
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
 
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
+YOUR_FACE_API_KEY = os.environ["YOUR_FACE_API_KEY"]
+YOUR_FACE_API_ENDPOINT = os.environ["YOUR_FACE_API_ENDPOINT"]
+
+face_client = FaceClient(
+    YOUR_FACE_API_ENDPOINT,
+    CognitiveServicesCredentials(YOUR_FACE_API_KEY)
+)
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -48,10 +59,31 @@ def handle_message(event):
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
+		try:
+				# メッセージIDを受け取る
+				message_id = event.message.id
+				# メッセージIDに含まれるmessage_contentを抽出する
+				message_content = line_bot_api.get_message_content(message_id)
+				# contentの画像データをバイナリデータとして扱えるようにする
+				image = BytesIO(message_content.content)
+
+				# Detect from streamで顔検出
+				detected_faces = face_client.face.detect_with_stream(image)
+				print(detected_faces)
+				# 検出結果に応じて処理を分ける
+				if detected_faces != []:
+						# 検出された顔の最初のIDを取得
+						text = detected_faces[0].face_id
+				else:
+						# 検出されない場合のメッセージ
+						text = "no faces detected"
+		except:
+				# エラー時のメッセージ
+				text = "error"
     # LINEチャネルを通じてメッセージを返答
-    line_bot_api.reply_message(
+		line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='画像です')
+        TextSendMessage(text=text)
     )
 
 
